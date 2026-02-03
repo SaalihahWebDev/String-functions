4fcfde7fb2e1681d95c00a587b0764f34cf1dd4e
SaalihahWebDev <bashirabanu91@gmail.com>
2026-02-03 05:02:05 -0800

    Add files via upload

---
+++ main.java
@@ -0,0 +1,18 @@
+class Main{
+    public static void main(String[] args){
+        String first="cod";
+        String second="ingal";
+        String codingal=first+second;
+        String codingalTrick="Welcome"+"to"+"codingal";
+        String codingalCapital=codingal.toUpperCase();
+        String codingalsmall=codingalCapital.toLowerCase();
+        int LengthOfCodingal=codingal.length();
+        int LengthOfCodingalTrick=codingalTrick.length();
+        int sum=LengthOfCodingalTrick+LengthOfCodingalTrick;
+        System.out.println(codingal);
+        System.out.println(codingalTrick);
+        System.out.println(codingalCapital);
+        System.out.println(codingalSmall);
+        System.out.println(sum);
+    }
+}
